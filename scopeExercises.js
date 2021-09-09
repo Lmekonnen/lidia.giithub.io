302906bf655d7c9ac92cb466fd00e0b1b862a219
Lmekonnen <89761413+Lmekonnen@users.noreply.github.com>
2021-09-09 11:03:50 -0500

    Update scopeExercises.js

--- scopeExercises.js
+++ scopeExercises.js
@@ -23,40 +23,38 @@ document.write(x);				//vi (x=1)
 
 
 //Ans: 	
-c(8, 9, 10); -> 10  //i 
-                8  //ii
-                8  //iii
-                9  //iv            
-document.write(b); -> 10
-document.write(x); -> 1
-//    * 
+// c(8, 9, 10); -> 10  //i 
+//                 8  //ii
+//                 8  //iii
+//                 9  //iv            
+// document.write(b); -> 10
+// document.write(x); -> 1
+
 // 2. What is the difference between a method and function?
-//    * Functions and methods are the same in JavaScript, but a method is a function, 
-//      which is a property of an object. A function can be called directly by its name but 
-//      methods are called by its object name and its method name using dot notation or square bracket notation.
+    //    * Functions and methods are the same in JavaScript, but a method is a function, 
+    //      which is a property of an object. A function can be called directly by its name but 
+    //      methods are called by its object name and its method name using dot notation or square bracket notation.
 
 // 3. What does 'this' refer to when used in a Java method?
-//    * ‘this’ refers to the current object in a method or in a constructor.
+    //    * ‘this’ refers to the current object in a method or in a constructor.
 // 4. What does 'this' refer to when used in a JavaScript method?
-//    * It has different values depending on where it is used:
-//      IN METHOD: It refers to owner object
-//      ALONE: It refers to global object
-//      IN A FUCNTION: It refers to global object
-//      IN AN EVENT: It refers to the element that recieved the event.
+    //    * It has different values depending on where it is used:
+    //      IN METHOD: It refers to owner object
+    //      ALONE: It refers to global object
+    //      IN A FUCNTION: It refers to global object
+    //      IN AN EVENT: It refers to the element that recieved the event.
 
 // 5. What does 'this' refer to when used in a JavaScript constructor function?
-//    * ‘this’ keyword refers to the instance of the constructor
+    //    * ‘this’ keyword refers to the instance of the constructor
 
-// 6. Assume object x is the prototype for object y in Javascript. Object x has a method f( ) containing keyword 'this'.
-// When f is called by x.f( ), what does 'this' refer to?
-//    * This - refers to x
+// 6. Assume object x is the prototype for object y in Javascript. Object x has a method f( ) containing keyword 'this'. When f is called by x.f( ), what does 'this' refer to?
+    //    * This - refers to x
 
 // 7. What is a free variable in JavaScript?
-//    * Free variable referred to a function that neither it's parameters nor local variable.
-//      There are lexical scope where a closure is created and free variable is used in JavaScript.
+    //    * Free variable referred to a function that neither it's parameters nor local variable.
+    //      There are lexical scope where a closure is created and free variable is used in JavaScript.
 
-// 8. Create an object that has properties with name = "fred" and major="music" and a property that is a function
-// that takes 2 numbers and returns the smallest of the two, or the square of the two if they are equal.
+// 8. Create an object that has properties with name = "fred" and major="music" and a property that is a function that takes 2 numbers and returns the smallest of the two, or the square of the two if they are equal.
 let obj = {
     name: "fred",
     major: "music",
@@ -82,8 +80,7 @@ function Employee(name, salary, position) {
 var emp1 = new Employee("Jhon", 9000, "Manager");
 var emp2 = new Employee("Ronaldo", 8000, "Supervisor");
 var emp3 = new Employee("Muller", 5000, "Software Engineer");
-// 10. Write a Javascript function that takes any number of input arguments and returns the product of the
-// arguments.
+// 10. Write a Javascript function that takes any number of input arguments and returns the product of the arguments.
 function productOfArguments() {
     let i;
     let productResult = 1;
