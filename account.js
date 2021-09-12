2ce4472620b6e1b5d6e562c25b9a188f7a4a651f
Lmekonnen <89761413+Lmekonnen@users.noreply.github.com>
2021-09-12 10:12:27 -0500

    update

--- account.js
+++ account.js
@@ -1,19 +1,9 @@
-// var module=(function accountInfo(){
-//     document.getElementById("display").src="back.gif";
-//     document.getElementById("name").value+document.getElementById("deposit").value;
-// })();
-// function accountInfo(){
-//     document.getElementById("display").innerHTML=document.getElementById("name").value;
-//     document.getElementById("display").innerHTML=document.getElementById("deposit").value;
-// }
-//setTimeout(accountInfo,1000);
-
-function showInput() {
-    document.getElementById('display').innerHTML = 
+var module=(function showInput() {
+    document.getElementById('display').innerHTML = "Account Name:" + " " +
     document.getElementById("user_input").value + " " +
     document.getElementById("acctype").value + " " +
     parseInt(document.getElementById("deposit").value);
-}
+})();
    
 window.onload = function(){
     var submits=document.getElementById("submit");
